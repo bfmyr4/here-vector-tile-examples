6eb44463fce604282c449ede9921168b109e982a
Brian Miller <brianmiller1@usicllc.com>
2023-11-03 14:02:39 +0100

    Adapt for WPF Map Size and Allow for easy comparision

--- mapbox/demo.js
+++ mapbox/demo.js
@@ -1,7 +1,10 @@
+var position = [37.678841, -97.317239];
+position.reverse();
+console.log(position);
+
 const map = new mapboxgl.Map({
    container: 'map',
-   style: 'https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=' + apikey,
-   center: [-122.431297, 37.773972], //Longitude, Latitude
-   zoom: 11
+   style: json,
+   center: position, //Longitude, Latitude
+   zoom: 19.5
 });
-
